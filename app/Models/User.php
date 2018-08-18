cd324835d4e0b671459173288020c3aae9697c0b
yyh-gl <yhonda.95.gl@gmail.com>
2018-08-18 14:06:50 +0900

    User::store追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,15 +30,19 @@ class User extends Authenticatable
     /**
      * The attributes that should be hidden for arrays.
      *
-     * @var array
+     * @param string $user_id
+     * @param string $name
+     * @param string $email
+     * @param string $avatar
      */
-    public static function store(array $user)
+    public static function store(string $user_id, string $name, string $email, string $avatar, string $password = '')
     {
-
-        // $user->token;
-        // $user->nickname;
-        // $user->name;
-        // $user->email;
-        // $user->avatar;
+        $user = new User();
+        $user->user_id = $user_id;
+        $user->name = $name;
+        $user->email = $email;
+        $user->avatar = $avatar;
+        $user->password = $password;
+        $user->save();
     }
 }
